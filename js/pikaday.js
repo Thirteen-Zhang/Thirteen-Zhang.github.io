0ca653b377791e0e503ee778a606ac3f096d6679
e <7836256+Thirteen-Zhang@user.noreply.gitee.com>
2022-12-26 19:36:21 +0800

    Site updated: 2022-12-26 19:36:19

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2022":{"10":{"24":true,"27":true,"28":true,"30":true},"11":{"1":true,"5":true,"8":true,"10":true,"11":true,"13":true,"15":true,"20":true,"21":true}}}';
+      var data = '{"2022":{"10":{"24":true,"27":true,"28":true,"30":true},"11":{"1":true,"5":true,"8":true,"10":true,"11":true,"13":true,"15":true,"20":true,"21":true,"23":true,"24":true,"25":true,"26":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
